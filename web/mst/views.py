d0255a7e64f5ba0ddcd0103c09589ac70ddb0e9d
liulu1998 <1274272851@qq.com>
2019-10-17 11:22:45 +0800

    完成初稿

--- web/mst/views.py
+++ web/mst/views.py
@@ -24,11 +24,11 @@ def upload_image(request):
 
     if request.method == 'POST':
         image = request.FILES.get('image', None)
-        print(image)
+        # print(image)
 
         # 选择的风格名称
         choice = request.POST['choice']
-        print('风格名称: ' + choice)
+        # print('风格名称: ' + choice)
 
         if image is None:
             return HttpResponse(json.dumps({'status': 1, 'message': '没有需要上传的文件!'}))
@@ -45,8 +45,8 @@ def upload_image(request):
             with open(path, 'wb') as f:
                 f.write(image.read())
 
-        print('上传成功')
-        print('存储路径为: ' + path)
+        # print('上传成功')
+        # print('存储路径为: ' + path)
 
         # TODO: 插入模型预测函数,修改返回值返回结果图片路径
         # 结果图片存储到/static/result/下面
@@ -56,20 +56,21 @@ def upload_image(request):
             "0": "starry_night.jpg",  # 星月夜
             "1": "the_scream.jpg",
             "2": "wave.jpg",
-            "3": "picasso_selfport1907.jpg"
+            "3": "picasso_selfport1907.jpg",
+            "4": "pencil.jpg"
         }
 
         # === 调用模型 === 
-        content_size = 512
+        content_size = 256   # 根据显存调整该参数
         root = "backend/"    # 模型部分的根文件夹
         style = root + f"images/styles/{style_[choice]}"  # 风格图片
         model = root + "models/21styles.model"  # 模型路径
-        output = "/static/result/output.jpg"  # 输出图片
+        output = "static/result/output.jpg"  # 输出图片
 
-        cmd = f"python {root}main.py eval --content-image {path} --style-image {style} --output-image {output} --model {model} --content-size {content_size}" 
+        cmd = f"python {root}main.py eval --content-image {path} --style-image {style} --output-image {output} --model {model} --content-size {content_size} --cuda 1" 
         os.system(cmd)
         
         # 结果文件名字,带后缀
         # result = '0.png'
 
-        return HttpResponse(json.dumps({'status': 0, 'message': '上传成功', 'result': output}))
+        return HttpResponse(json.dumps({'status': 0, 'message': '上传成功', 'result': "output.jpg"}))

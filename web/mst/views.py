5b12cacab6a74fd6780df0482aac24c3840339ca
xyy <1499488968@qq.com>
2019-10-19 10:54:01 +0800

    自动检测和创建上传和输出文件夹

--- web/mst/views.py
+++ web/mst/views.py
@@ -22,46 +22,42 @@ def upload_image(request):
     接受上传图片和选择的风格
     """
     if request.method == 'GET':
-        upload_path = os.path.join(STATICFILES_DIRS[0], 'upload/')
-        if not os.path.isdir(upload_path):
-            os.makedirs(upload_path)
-
-        result_path = os.path.join(STATICFILES_DIRS[0], 'result/')
-        if not os.path.isdir(result_path):
-            os.makedirs(result_path)
-
         return render(request, 'index.html')
 
     if request.method == 'POST':
         image = request.FILES.get('image', None)  # 上传的图片
 
+        image_name = get_random() + '-' + image.name  # 生成图片和上传图片的名称
+
         choice = request.POST['choice']  # 选择的风格名称
-        print(STATICFILES_DIRS[0])
 
         # 上传图片
         if image is None:
             return HttpResponse(json.dumps({'status': 1, 'message': '没有需要上传的文件!'}))
         else:
-            result = get_random() + '-' + image.name  # 返回值,也是生成图片的名称
-            path = os.path.join(STATICFILES_DIRS[0], 'upload/' + result)  # 上传的文件存储路径
-            if not save_file(image, path):
+
+            if not save_file(image, image_name):
                 return HttpResponse(json.dumps({'status': 1, 'message': '上传文件失败!'}))
 
         # 调用模型
-        if not test_model(choice, result, path):
+        if not test_model(choice, image_name):
             return HttpResponse(json.dumps({'status': 1, 'message': '生成图片失败'}))
 
-        return HttpResponse(json.dumps({'status': 0, 'message': '上传成功', 'result': result}))
+        return HttpResponse(json.dumps({'status': 0, 'message': '上传成功', 'result': image_name}))
 
 
-def test_model(choice, result, path):
+def test_model(choice, image_name):
     """
     调用模型
     :param choice: 选择的风格
-    :param result: 调用模型生成的图片名称
-    :param path: 上传的图片路径
+    :param image_name: 调用模型生成的图片名称
     :return:
     """
+    # 判断输出文件夹是否存在,不存在则创建
+    output_dir = 'static/result/'
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
     # choice 与风格图片的映射
     style_ = {
         "0": "starry_night.jpg",  # 星月夜
@@ -76,19 +72,26 @@ def test_model(choice, result, path):
     root = "backend/"  # 模型部分的根文件夹
     style = root + f"images/styles/{style_[choice]}"  # 风格图片
     model = root + "models/21styles.model"  # 模型路径
-    output = "static/result/" + result  # 输出图片
+    path = 'static/upload' + image_name  # 上传图片
+    output = "static/result/" + image_name  # 输出图片
 
     cmd = f"python {root}main.py eval --content-image {path} --style-image {style} --output-image {output} --model {model} --content-size {content_size} --cuda 1"
     os.system(cmd)
     return True
 
 
-def save_file(file, path):
+def save_file(file, image_name):
     """
     保存文件
     :param file: 文件
-    :param path: 保存路径
+    :param image_name: 保存路径
     """
+    upload_dir = 'static/upload/'
+    if not os.path.isdir(upload_dir):
+        os.makedirs(upload_dir)
+
+    path = upload_dir + image_name
+
     if file.multiple_chunks():  # 判断上传文件大于2.5MB的大文件
         file_yield = file.chunks()  # 迭代写入文件
         with open(path, 'wb') as f:
